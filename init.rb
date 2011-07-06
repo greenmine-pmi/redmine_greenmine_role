ea3b76c48d9a356bccaf200cb997969622502b84
Dmitry Ovcharenko <ovcharenkodmitry@gmail.com>
2011-07-06 12:18:00 +0600

    Исправления ошибок при разделении плагинов

--- init.rb
+++ init.rb
@@ -2,6 +2,8 @@ require 'redmine'
 
 require 'issues_helper_patch'
 require 'application_helper_patch'
+require 'redmine_export_pdf_patch' # overwrite base class
+
 
 Redmine::Plugin.register :redmine_greenmine_role do
   name 'Redmine Greenmine role plugin'
@@ -10,6 +12,18 @@ Redmine::Plugin.register :redmine_greenmine_role do
   version '0.0.1'
   url 'https://github.com/greenmine-pmi/redmine_greenmine_role'
   author_url 'http://greenmine-pmi.blogspot.com/'
+
+ project_module :issue_tracking do
+    permission(:issue_move_up_down, {})
+    permission(:issue_edit_start_date, {})
+    permission(:issue_edit_due_date, {})
+    permission(:issue_edit_priority, {})
+    permission(:issue_edit_assigned_to, {})
+    permission(:issue_edit_done_ratio, {})
+    permission(:issue_edit_status, {})
+  end
+
+
 end
 
 require 'dispatcher'
